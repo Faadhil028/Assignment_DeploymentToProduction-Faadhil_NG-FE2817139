83faca2517b3ee365497b063638cf2fdcb5ec1f4
Faadhil Nurdi Gusmadi <l200200028@student.ums.ac.id>
2022-12-15 14:51:28 +0700

    Adding assignment files

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Faadhil Nurdi Gusmadi" // TODO: replace this
+const studentId = "FE2817139" // TODO: replace this
+
+export { url, studentName, studentId }
